1b38abffef351975509f4942e8a98096064124b7
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-20 17:41:00 +0900

    worker 만들기 전

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-sudo docker run -d \
-	--name mnist-mariadb \
-	-e MARIADB_USER=mnist \
-	--env MARIADB_PASSWORD=1234 \
-	--env MARIADB_DATABASE=mnistdb \
-	--env MARIADB_ROOT_PASSWORD=my-secret-pw \
-	-p 53306:3306 \
-	mariadb:latest
+service cron start;uvicorn main:app --host 0.0.0.0 --port 8080 --reload
