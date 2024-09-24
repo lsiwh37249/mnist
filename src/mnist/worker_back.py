a3286335f7d9bb6b967aca061116e1f815e75268
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-24 17:05:16 +0900

    fix err

--- src/mnist/worker_back.py
+++ src/mnist/worker_back.py
@@ -34,28 +34,29 @@ def send_line(messages):
     response = requests.post('https://notify-api.line.me/api/notify', headers=headers, data = info)
 
 def run():
-    # STEP 1
-    # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 갖여오기
-    num,image_path = one()
+        # STEP 1
+        # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 갖여오기
+    try:
+        num,image_path = one()
     
-    if num is None:
-      print(f"{jigeum.seoul.now()} - job is None")
-      return
-    
-    # STEP 2
-    # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
-    #image_path = "/home/kim1/code/mnist/img/2eba341a-d3dd-47dd-8fba-b446158bfa79.png"
-    predict_result = predict_digit(image_path)
-    print(f"predict_result : {predict_result}")
-    prediction_time = now_seoul()
-    prediction_model = "n22"
+        # STEP 2
+        # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
+        #image_path = "/home/kim1/code/mnist/img/2eba341a-d3dd-47dd-8fba-b446158bfa79.png"
+        predict_result = predict_digit(image_path)
+        print(f"predict_result : {predict_result}")
+        prediction_time = now_seoul()
+        prediction_model = "n22"
 
-    # 동시에 prediction_model, prediction_time 도 업데이트
-    predict_set = (predict_result, prediction_model,prediction_time, num)
-    result = update_result(*predict_set)
-    result_pr = result[0]['prediction_result']
-    # STEP 3
-    # LINE 으로 처리 결과 전송
-    datetime = now_seoul()
-    print({"now" :f"{datetime}"})
-    send_line(result_pr)
+        # 동시에 prediction_model, prediction_time 도 업데이트
+        predict_set = (predict_result, prediction_model,prediction_time, num)
+        result = update_result(*predict_set)
+        result_pr = result[0]['prediction_result']
+        # STEP 3
+        # LINE 으로 처리 결과 전송
+    except:
+        result_pr = "처리할 이미지가 필요합니다."
+        
+    finally:
+        datetime = now_seoul()
+        print({"now" :f"{datetime}"})
+        send_line(result_pr)
